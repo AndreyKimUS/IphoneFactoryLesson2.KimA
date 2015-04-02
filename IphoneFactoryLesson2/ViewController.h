f90fadf2bcb6e55ef4dd03512b70b51ac4f35138
AndreyKimUS <admin@sakhparfum.ru>
2015-04-03 01:46:31 +0300

    Initial Commit

---
+++ IphoneFactoryLesson2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IphoneFactoryLesson2
+//
+//  Created by Андрей Ким on 03.04.15.
+//  Copyright (c) 2015 Андрей Ким. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IphoneFactoryLesson2Tests/IphoneFactoryLesson2Tests.m
@@ -0,0 +1,40 @@
+//
+//  IphoneFactoryLesson2Tests.m
+//  IphoneFactoryLesson2Tests
+//
+//  Created by Андрей Ким on 03.04.15.
+//  Copyright (c) 2015 Андрей Ким. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface IphoneFactoryLesson2Tests : XCTestCase
+
+@end
+
+@implementation IphoneFactoryLesson2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
